01d436dd1624c76765dbd12013ab4a07cd59eee1
Haegeman Cédric <79855780+Cedric60128@users.noreply.github.com>
2021-04-26 12:22:57 +0200

    Update LFBfinal.py

--- LFBfinal.py
+++ LFBfinal.py
@@ -380,7 +380,7 @@ if submit:
 
 
     st.markdown(
-        """<a style='display: block; text-align: center;' href="javascript:history.back()">BACK</a>
+        """<a style='display: block; text-align: center;' href="https://share.streamlit.io/cedric60128/lfb/main/LFBfinal.py">BACK</a>
         """,
         unsafe_allow_html=True,
     )
