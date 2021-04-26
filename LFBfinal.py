77702e4158acc807755dac7a9768616bdc92fc17
Haegeman Cédric <79855780+Cedric60128@users.noreply.github.com>
2021-04-26 12:28:45 +0200

    Update LFBfinal.py

--- LFBfinal.py
+++ LFBfinal.py
@@ -384,3 +384,7 @@ if submit:
         """,
         unsafe_allow_html=True,
     )
+    
+    
+link_number = st.number_input("What subheading do you want to go to?", value=50)
+st.markdown(f"<a href='#linkto_{link_number}'>Link to {link_number}</a>", unsafe_allow_html=True)
